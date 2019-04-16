95c7f440518558c3b1fd84431eebd298c9a18547
AngelVil92 <angelvil92@gmail.com>
2019-04-16 10:45:04 +0200

    Ejemplos de event binding (con $event) y two-way binding

--- angular/proyecto/src/app/app.module.ts
+++ angular/proyecto/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
@@ -7,6 +8,9 @@ import { OtroComponenteComponent } from './otro-componente/otro-componente.compo
 import { CmpDatabindingComponent } from './cmp-databinding/cmp-databinding.component';
 import { CmpStrInterpolationComponent } from './cmp-databinding/cmp-str-interpolation/cmp-str-interpolation.component';
 import { CmpPropertyBindingComponent } from './cmp-databinding/cmp-property-binding/cmp-property-binding.component';
+import { CmpEventBindingComponent } from './cmp-databinding/cmp-event-binding/cmp-event-binding.component';
+import { CmpTwoWayBindingComponent } from './cmp-databinding/cmp-two-way-binding/cmp-two-way-binding.component';
+import { MiChuWayBindingComponent } from './cmp-databinding/mi-chu-way-binding/mi-chu-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,14 @@ import { CmpPropertyBindingComponent } from './cmp-databinding/cmp-property-bind
     OtroComponenteComponent,
     CmpDatabindingComponent,
     CmpStrInterpolationComponent,
-    CmpPropertyBindingComponent
+    CmpPropertyBindingComponent,
+    CmpEventBindingComponent,
+    CmpTwoWayBindingComponent,
+    MiChuWayBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
