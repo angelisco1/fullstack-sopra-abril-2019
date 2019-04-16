c6a79a57ef8278e23c7cc39ee26ecb66db4666a0
AngelVil92 <angelvil92@gmail.com>
2019-04-16 09:54:47 +0200

    Ejemplos de string interpolation y property binding (one-way-binding)

--- angular/proyecto/src/app/app.module.ts
+++ angular/proyecto/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
 import { OtroComponenteComponent } from './otro-componente/otro-componente.component';
+import { CmpDatabindingComponent } from './cmp-databinding/cmp-databinding.component';
+import { CmpStrInterpolationComponent } from './cmp-databinding/cmp-str-interpolation/cmp-str-interpolation.component';
+import { CmpPropertyBindingComponent } from './cmp-databinding/cmp-property-binding/cmp-property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiPrimerComponenteComponent,
-    OtroComponenteComponent
+    OtroComponenteComponent,
+    CmpDatabindingComponent,
+    CmpStrInterpolationComponent,
+    CmpPropertyBindingComponent
   ],
   imports: [
     BrowserModule
