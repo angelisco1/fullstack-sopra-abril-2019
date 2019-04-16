42d44500d7d90035a5c445c3858a2ead6a1da239
AngelVil92 <angelvil92@gmail.com>
2019-04-16 09:34:14 +0200

    Ejemplo inicial de Angular 7 con 2 componentes

---
+++ angular/proyecto/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-primer-componente',
+  templateUrl: './mi-primer-componente.component.html',
+  styleUrls: ['./mi-primer-componente.component.css']
+})
+export class MiPrimerComponenteComponent implements OnInit {
+
+  ngOnInit() { }
+
+}
