9f8db7041bd126b07c8b239fd27b1eaa48f01e4e
AngelVil92 <angelvil92@gmail.com>
2019-04-22 16:45:10 +0200

    Ejemplo usar otro servicio usando useClass

--- angular/proyecto/src/app/cmp-servicios/mensajes-consola.service.ts
+++ angular/proyecto/src/app/cmp-servicios/mensajes-consola.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 
+class MensajesConsolaFechaService {
+
+  constructor() { }
+
+  mostrarMensaje(msg: string): void {
+    console.log(new Date().toDateString() + ' MSG: ' + msg);
+  }
+}
+
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  useClass: MensajesConsolaFechaService
 })
 export class MensajesConsolaService {
 
