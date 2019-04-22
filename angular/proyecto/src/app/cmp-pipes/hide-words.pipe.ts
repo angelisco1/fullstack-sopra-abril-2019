c2aa19ce58c09aa7a2be3a71fdae8cb6d7941772
AngelVil92 <angelvil92@gmail.com>
2019-04-22 10:00:23 +0200

    Ejercicio pipe ocultar palabras de un string

--- angular/proyecto/src/app/cmp-pipes/hide-words.pipe.ts
+++ angular/proyecto/src/app/cmp-pipes/hide-words.pipe.ts
@@ -5,10 +5,39 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HideWordsPipe implements PipeTransform {
 
-  // TODO: {{ "Minions ipsum bee do bee do bee do butt" | hideWords:['bee', 'butt'] }} => "Minions ipsum * do * do * do *"
-  // TODO: {{ "Minions ipsum bee do bee do bee do butt" | hideWords:['bee', 'butt']:'$' }} => "Minions ipsum $ do $ do $ do $"
-  transform(value: any, args?: any): any {
-    return null;
+  // {{ "Minions ipsum bee do bee do bee do butt" | hideWords:['bee', 'butt'] }} => "Minions ipsum * do * do * do *"
+  // {{ "Minions ipsum bee do bee do bee do butt" | hideWords:['bee', 'butt']:'$' }} => "Minions ipsum $ do $ do $ do $"
+  transform(value: string, arg1: Array<string>, arg2?: string): string {
+    // return this.conIncludes(value, arg1, arg2);
+    return this.conRegExp(value, arg1, arg2);
+  }
+
+  conRegExp(value: string, arg1: Array<string>, arg2?: string): string {
+    let res = value;
+    let charOculto = arg2 ? arg2 : '*';
+
+    for (let palabraAOcultar of arg1) {
+      let reg = new RegExp(palabraAOcultar, 'g');
+      res = res.replace(reg, charOculto.repeat(palabraAOcultar.length));
+    }
+
+    return res;
+  }
+
+  conIncludes(value: string, arg1: Array<string>, arg2?: string): string {
+    let res = [];
+    let charOculto = arg2 ? arg2 : '*';
+    let arrPalabras = value.split(" ");
+
+    for (let palabra of arrPalabras) {
+      if (arg1.includes(palabra)) {
+        res.push(charOculto.repeat(palabra.length));
+      } else {
+        res.push(palabra);
+      }
+    }
+
+    return res.join(" ");
   }
 
 }
